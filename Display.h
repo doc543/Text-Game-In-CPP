5f328ac15d2b89757ebf2a038a0839a360572c5f
Eric <eandersen208@gmail.com>
2017-08-03 18:36:50 -0600

    I kinda broke git, I think it is working now

--- Display.h
+++ Display.h
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-#include <iostream>
-#include <string>
-=======
->>>>>>> Stashed changes
 /*******************************************************	
 /	Display 
 /	This class encapisilates the ncurses libary for eazy
@@ -13,10 +6,6 @@
 *******************************************************/
 #include <iostream> //should I put these in the .h file and the .cpp file?
 #include <string> 
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 
 #ifndef DISP_H 
 #define DISP_H 
@@ -27,28 +16,6 @@ class Display {
 		int consoleYSize;
     public:
 		Display();
-<<<<<<< Updated upstream
-		~Display();
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-		
-		//functions
-		void refresh();
-		
-		//getters
-		std::string getInput(int x0, int y0);
-		
-		//setters
-		void printBody(int x0, int y0, std::string body);
-		void drawBorder(char borderChar);
-        void drawHorzLine(int y0, char fillChar);
-		void drawVertLine(int x0, char fillChar);
-<<<<<<< Updated upstream
-=======
-				
-		~Display();
-=======
 		~Display();
 		
 		//functions
@@ -62,8 +29,6 @@ class Display {
 		void drawBorder(char borderChar);
         void drawHorzLine(int y0, char fillChar);
 		void drawVertLine(int x0, char fillChar);
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -14,27 +14,6 @@ int main()
 {
    Display disp;
    
-<<<<<<< Updated upstream
-   disp.drawHorzLine(5, '-');
-=======
-<<<<<<< Updated upstream
-   disp.drawHorzLine(10, '-');
->>>>>>> Stashed changes
-   disp.drawVertLine(10, '|');
-   disp.drawBorder('x');
-   disp.printBody(2, 3, "Halla halla get dalla");
-   
-   
-   string str;
-   str = disp.getInput(3, 5);
-   disp.printBody(7, 6, str);
-	
-<<<<<<< Updated upstream
-   disp.printBody(8, 5, "GIT GUD");
-=======
-	
-   refresh();			/* Print it on to the real screen */
-=======
    disp.drawHorzLine(5, '-');
    disp.drawVertLine(10, '|');
    disp.drawBorder('x');
@@ -46,8 +25,6 @@ int main()
    disp.printBody(7, 6, str);
 	
    disp.printBody(8, 5, "GIT GUD");
->>>>>>> Stashed changes
->>>>>>> Stashed changes
    getch();			/* Wait for user input */
   
    return 0;
